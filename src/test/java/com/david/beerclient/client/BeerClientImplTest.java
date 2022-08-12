51f8576e101d3f113e5e561d28f84a963f4dedaf
David Zarud <davidzarud@gmail.com>
2022-08-12 13:04:38 +0300

    Add query params, add tests

--- src/test/java/com/david/beerclient/client/BeerClientImplTest.java
+++ src/test/java/com/david/beerclient/client/BeerClientImplTest.java
@@ -4,6 +4,7 @@ import com.david.beerclient.config.WebClientConfig;
 import com.david.beerclient.dao.model.BeerPagedList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -33,6 +34,73 @@ class BeerClientImplTest {
 
         assertThat(beerPagedList).isNotNull();
         assertThat(beerPagedList.getContent().size()).isGreaterThan(0);
+    }
+
+    @Test
+    void listBeersPageSize10() {
+
+        Mono<BeerPagedList> beerPagedListMono = beerClient.listBeers(1, 10, null, null, null);
+
+        BeerPagedList beerPagedList = beerPagedListMono.block();
+
+        assertThat(beerPagedList).isNotNull();
+        assertThat(beerPagedList.getContent().size()).isEqualTo(10);
+    }
+
+    @Test
+    void listBeersEmpty() {
+
+        Mono<BeerPagedList> beerPagedListMono = beerClient.listBeers(10, 20, null, null, null);
+
+        BeerPagedList beerPagedList = beerPagedListMono.block();
+
+        assertThat(beerPagedList).isNotNull();
+        assertThat(beerPagedList.getContent().size()).isEqualTo(0);
+    }
+
+    @Test
+    void listWheatBeers() {
+
+        Mono<BeerPagedList> beerPagedListMono = beerClient.listBeers(null, null, null, "WHEAT", true);
+
+        BeerPagedList beerPagedList = beerPagedListMono.block();
+
+        beerPagedList.forEach(beer -> assertThat(beer.getBeerStyle().equals("WHEAT")));
+        assertThat(beerPagedList).isNotNull();
+        assertThat(beerPagedList.getContent().size()).isGreaterThan(0);
+    }
+
+    @Test
+    void listWheatBeers2Pages() {
+
+        // First Page
+        Mono<BeerPagedList> beerPagedListMono = beerClient.listBeers(0, 2, null, "WHEAT", true);
+
+        BeerPagedList beerPagedList = beerPagedListMono.block();
+
+        beerPagedList.forEach(beer -> assertThat(beer.getBeerStyle().equals("WHEAT")));
+        assertThat(beerPagedList).isNotNull();
+        assertThat(beerPagedList.getContent().size()).isEqualTo(2);
+        assertThat(beerPagedList.getPageable().getPageNumber()).isEqualTo(0);
+        assertThat(beerPagedList.getPageable().getPageSize()).isEqualTo(2);
+
+        // Second Page
+        beerPagedListMono = beerClient.listBeers(1, 2, null, "WHEAT", true);
+
+        beerPagedList = beerPagedListMono.block();
+
+        beerPagedList.forEach(beer -> assertThat(beer.getBeerStyle().equals("WHEAT")));
+        assertThat(beerPagedList).isNotNull();
+        assertThat(beerPagedList.getContent().size()).isEqualTo(2);
+        assertThat(beerPagedList.getPageable().getPageNumber()).isEqualTo(1);
+        assertThat(beerPagedList.getPageable().getPageSize()).isEqualTo(2);
+
+        // Third Non-Existent Page
+        beerPagedListMono = beerClient.listBeers(2, 2, null, "WHEAT", true);
+
+        beerPagedList = beerPagedListMono.block();
+
+        assertThat(CollectionUtils.isEmpty(beerPagedList.getContent()));
     }
 
     @Test
